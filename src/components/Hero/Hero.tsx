39fdc9b0669a85eae86ce2083a92777f6b594d5f
aliMohamad98 <eng.alimoh98m@gmail.com>
2025-01-11 17:09:48 +0300

    fix deploy

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,16 @@
 import { Fade } from "react-awesome-reveal";
 import { FaArrowRight } from "react-icons/fa6";
-
+import heroBgImage from "./../../assets/hero/bg.jpg";
 const Hero = () => {
   return (
     <main className="max-w-[1920px] mx-auto bg-white overflow-hidden">
       <div
         className="xl:bg-center xl:bg-repeat-y fixed top-0 left-0 bottom-0 right-0 z-10"
-        style={{ backgroundImage: 'url("./src/assets/grid.png")' }}
+        style={{ backgroundImage: "url(./src/assets/grid.png)" }}
       ></div>
       <section
         className="h-[640px] xl:h-[840px] bg-center lg:bg-cover bg-no-repeat bg-fixed xl:rounded-bl-[290px] relative z-20"
-        style={{ backgroundImage: 'url("./src/assets/hero/bg.jpg")' }}
+        style={{ backgroundImage: `url(${heroBgImage})` }}
       >
         <div className="container mx-auto h-full flex items-center justify-center xl:justify-start ">
           <div className="w-[567px] flex flex-col items-center xl:text-left lg:items-start">
